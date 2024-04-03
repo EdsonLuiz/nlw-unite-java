0f9fe818f81daea85bf7725e6d14b9773f482458
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2024-04-03 20:06:08 -0300

    feat: add endpoint /v1/{eventId}/attendees

--- src/main/java/com/edson/passin/controllers/EventController.java
+++ src/main/java/com/edson/passin/controllers/EventController.java
@@ -1,5 +1,7 @@
 package com.edson.passin.controllers;
 
+import com.edson.passin.dto.attendee.AttendeeIdDTO;
+import com.edson.passin.dto.attendee.AttendeeRequestDTO;
 import com.edson.passin.dto.attendee.AttendeesResponseDTO;
 import com.edson.passin.dto.event.EventIdDTO;
 import com.edson.passin.dto.event.EventRequestDTO;
@@ -34,6 +36,13 @@ public class EventController {
     public ResponseEntity<AttendeesResponseDTO> getEventAttendees(@PathVariable String id) {
         AttendeesResponseDTO attendeesResponseDTO = this.attendeeService.getEventsAttendee(id);
         return ResponseEntity.ok(attendeesResponseDTO);
+    }
+
+    @PostMapping("/{eventId}/attendees")
+    public ResponseEntity<AttendeeIdDTO> registerParticipant(@PathVariable String eventId, @RequestBody AttendeeRequestDTO body, UriComponentsBuilder uriComponentsBuilder) {
+        AttendeeIdDTO attendeeIdDTO = this.eventservice.registerAttendeeOnEvent(eventId, body);
+        var uri = uriComponentsBuilder.path("/v1/attendees/{attendeeId}/badge").buildAndExpand(attendeeIdDTO.attendeeId()).toUri();
+        return ResponseEntity.created(uri).body(attendeeIdDTO);
     }
 
 }
