f1c4dc8f7ccefe5b4fe24e1bdc1cb25575a6848b
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2024-04-03 19:59:03 -0300

    feat: add endpoint /v1/attendees/{attendeeId}/badge

--- src/main/java/com/edson/passin/controllers/AttendeeController.java
+++ src/main/java/com/edson/passin/controllers/AttendeeController.java
@@ -1,15 +1,25 @@
 package com.edson.passin.controllers;
 
+import com.edson.passin.dto.attendee.AttendeeBadgeResponseDTO;
+import com.edson.passin.services.AttendeeService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
-@RequestMapping("/attendees")
+@RequestMapping("/v1/attendees")
+@RequiredArgsConstructor
 public class AttendeeController {
-    @GetMapping
-    public ResponseEntity<String> getTest() {
-        return ResponseEntity.ok("success");
+    private final AttendeeService attendeeService;
+    @GetMapping("/{attendeeId}/badge")
+    public ResponseEntity<AttendeeBadgeResponseDTO> getAttendeeBadge(@PathVariable String attendeeId, UriComponentsBuilder uriComponentsBuilder) {
+        AttendeeBadgeResponseDTO attendeeBadgeResponseDTO = this.attendeeService.getAttendeeBadge(attendeeId, uriComponentsBuilder);
+        return ResponseEntity.ok(attendeeBadgeResponseDTO);
     }
+
+
 }
